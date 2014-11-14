8011742db31b9c6e43d1e91fdcd5ccce795c96a0
Jaehyun Jang <jaehyun.jang@kiswe.com>
2014-11-13 23:03:11 -0500

    Repo Notification

---
+++ README.md
@@ -0,0 +1,23 @@
+# Git Repository Notification (Mac only)
+---
+A Simple Notification Manager for git repositories.
+You should register this shell script on crontab.
+This script does not permit space for directory names.
+
+# Usage
+---
+Ex)
+A
+\- Repository_1
+ \- Repository_2
+  \- ...
+
+in .folder file :
+/path/to/A (absolute path to A)
+
+and setup .repos to folder A:
+./Repository_1
+./Repository_2
+...
+
+Enjoy.
---
+++ update.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#Notification for each repo
+InitialNoti() {
+	GITFILE="/.git"
+	git --git-dir=$1$GITFILE fetch origin
+	RESLOG=$(git --git-dir=$1$GITFILE log HEAD..origin/master --oneline)
+	if [[ "${RESLOG}" != "" ]]; then
+		/usr/bin/osascript -e "display notification \"need to update\" with title \"Kiswe Mobile\" subtitle \"$1\""
+		echo $RESLOG
+	else
+		echo "_"
+	fi
+}
+
+Notification() {
+	GITFILE="/.git"
+	git --git-dir=$1$GITFILE fetch origin
+	RESLOG=$(git --git-dir=$1$GITFILE log HEAD..origin/master --oneline)
+	if [[ $RESLOG=$2 ]]; then
+		echo "_"
+	else
+		if [[ "${RESLOG}" != "" ]]; then
+			/usr/bin/osascript -e "display notification \"need to update\" with title \"Kiswe Mobile\" subtitle \"$1\""
+			echo $RESLOG
+		else
+			echo "_"
+		fi
+
+	fi
+}
+
+while read folder; do
+	cd $folder
+	i=0
+	if [ -a "./.hashes" ]; then
+		while read -r repo && read -r hash <&3; do
+			RETVAL=$(Notification $repo $hash)
+			hashes[$i]=$RETVAL
+			i=$(($i+1))
+		done < "./.repos" 3<"./.hashes"
+	else
+		while read -r repo; do
+			RETVAL=$(InitialNoti $repo)
+			hashes[$i]=$RETVAL
+			i=$(($i+1))
+		done < "./.repos"
+	fi
+	printf "%s\n" "${hashes[@]}" > ".hashes"
+done < "./.folders"
